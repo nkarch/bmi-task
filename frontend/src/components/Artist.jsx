306fb0e7cead78ab6d0c218eac6639b1e050bbee
Nick Karch <nickkarch@Nicks-MacBook-Pro.local>
2024-01-04 23:21:01 -0500

    Make Artist component

---
+++ frontend/src/components/Artist.jsx
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+
+function Artist({ name, nationality, age, active }) {
+    return (
+        <div
+            className={`bg-slate-200 text-slate-950 py-6 px-14 rounded ${
+                active && "active"
+            }`}
+        >
+            <dl>
+                <div className='def flex gap-2'>
+                    <dt>Name: </dt>
+                    <dd>{name || "Unknown"}</dd>
+                </div>
+
+                <div className='def flex gap-2'>
+                    <dt>Nationality: </dt>
+                    <dd>{nationality || "Unknown"}</dd>
+                </div>
+
+                <div className='def flex gap-2'>
+                    <dt>Age: </dt>
+                    <dd>{age || "Unknown"}</dd>
+                </div>
+            </dl>
+        </div>
+    );
+}
+
+Artist.propTypes = {
+    name: PropTypes.string,
+    nationality: PropTypes.string,
+    age: PropTypes.number,
+    active: PropTypes.bool,
+};
+export default Artist;
